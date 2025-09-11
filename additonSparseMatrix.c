1cdb27655c461288e089d58b1cb0b3744ccc8a1d
RoseMary007 <rosemaryjoe2028@cs.sjcetpalai.ac.in>
2025-09-11 16:02:24 +0530

    Add files via upload

---
+++ additonSparseMatrix.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+int main(){
+int row,col;
+int i,j;
+int sum;
+printf("enter your row size");
+scanf("%d",&row);
+printf("enter your column size");
+scanf("%d",&col);
+int a[row][col];
+int b[row][col];
+int c[row][col];
+int count=0;
+int s[100][3];
+
+
+
+printf("enter your first array elements\n");
+for(i=0;i<row;i++){
+for(j=0;j<col;j++)
+{
+scanf("%d",&a[i][j]);
+}}
+
+
+
+printf("enter your second array elements\n");
+for(i=0;i<row;i++){
+for(j=0;j<col;j++)
+{
+scanf("%d",&b[i][j]);
+}}
+
+
+
+for(i=0;i<row;i++){
+for(j=0;j<col;j++){
+sum=a[i][j]+b[i][j];
+c[i][j]=sum;
+}}
+
+
+
+
+s[0][1]=i;
+s[0][0]=j;
+for(i=0;i<row;i++){
+for(j=0;j<col;j++){
+if(c[i][j]!=0){
+count++;
+s[count][1]=i;
+s[count][0]=j;
+s[count][2]=c[i][j];
+}}}
+s[0][2]=count;
+
+
+
+printf("SPARSE MATRIX 1\n");
+for(i=0;i<row;i++){
+for(j=0;j<col;j++){
+printf("%d\t",a[i][j]);
+}
+printf("\n");
+}
+printf("SPARSE MATRIX 2\n");
+for(i=0;i<row;i++){
+for(j=0;j<col;j++){
+printf("%d\t",b[i][j]);
+}
+printf("\n");
+}
+printf("SUM OF MATRIX\n");
+for(i=0;i<row;i++){
+for(j=0;j<col;j++){
+printf("%d\t",c[i][j]);
+}
+printf("\n");
+}
+
+
+
+printf(" transpose representation matrix\n");
+for(i=0;i<=count;i++){
+for(j=0;j<3;j++){
+printf("%d\t",s[i][j]);
+}
+printf("\n");
+}
+
+return 0;
+}
